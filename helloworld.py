2dbc77aa446138c892756fd9a73b3c80d0a6319e
Anubhav-Sah <anupsah2005@gmail.comgit>
2025-01-18 12:10:04 +0530

    About Python

---
+++ helloworld.py
@@ -0,0 +1,19 @@
+#********************About Python********************
+
+# Python is a high-level, interpreted programming language that is widely used for various purposes such as web development
+# Created by Guido Van Russum in 1989-1991
+# Python is dynamically type programming language
+# Python is an object-oriented programming language
+# Python is a multi-paradigm language, meaning it supports multiple programming paradigms such as
+# 1. Object-Oriented Programming (OOP)
+# 2. Functional Programming (FP)
+# 3. Imperative Programming (IP)
+# 4. Procedural Programming (PP)
+# 5. Scripting Programming (SP)
+# Python is a cross-platform language, meaning it can run on multiple operating systems such as Windows, Linux, and macOS.
+# Python is an open-source language, meaning it is free to use and distribute.
+# Python has a vast number of libraries and frameworks that make it suitable for various tasks such as web development, data analysis, machine learning, automation, and more.
+# Python is easy to learn and has a simple syntax, making it a great language for beginners.
+# Python is a versatile language that can be used for a wide range of applications, from simple scripts to complex enterprise-level systems.
+# Python is a rapidly evolving language, with new features and libraries being added regularly.
+
