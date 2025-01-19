665784baedbcd0f4c56f64582394e6ab981f7a6e
Anubhav-Sah <anupsah2005@gmail.comgit>
2025-01-19 14:38:06 +0530

    input function in python

---
+++ inputInPython.py
@@ -0,0 +1,12 @@
+ # syntax to take input in python
+#  variable_name = input("<some message>")
+# if you take input in python its data type will be str i.e., if you enter a number then also it will be as a string
+
+name = input("Enter your name = ")
+print(f"My name is {name}")
+
+a=input("Enter the first number : ")
+b= input ("Enter the second number : ")
+
+print("Addition normally",a+b)
+print("Addition after type casting",int(a)+int(b))
