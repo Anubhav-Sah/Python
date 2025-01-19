005509ac67bdd3ca00106b1872ba3c4a4ed15b6d
Anubhav-Sah <anupsah2005@gmail.comgit>
2025-01-19 13:52:55 +0530

    Operators and Simple calculator

---
+++ operators.py
@@ -0,0 +1,28 @@
+#*************************Operators*************************
+#Python has different types of Operators which can be categorized into following types:
+#1. Arithmetic Operators
+#2. Comparison Operators
+#3. Assignment Operators
+#4. Logical Operators
+#5. Membership Operators
+#6. Identity Operators
+#7. Bitwise Operators
+
+
+#*************************Arithmetic Operators*************************
+#Arithmetic Operators are used to perform mathematical operations like addition, subtraction, multiplication, division etc. 
+# Addition Operator -> "+" -> 15+4
+# Substraction Operator -> "-" -> 15-4
+# Multiplication Operator -> "*" -> 15*4
+# Division Operator -> "/" -> 15/4
+# Modulus Operator -> "%" -> 15%4
+# Float Division -> "//" -> 15//4
+# Exponentiation Operator -> "**" -> 15**4
+
+print("Addition",15+4)
+print("Substraction",15-4)
+print("Multiplication",15*4)
+print("Division",5/3)
+print("Remainder",5%3)
+print("Exponent",15**4)
+print("Exponent",5//3)
---
+++ simpleCalculator.py
@@ -0,0 +1,10 @@
+
+firstNumber = int(input("Enter the first number: "))
+secondNumber = int(input("Enter the second number: "))
+
+print(f"Addition of {firstNumber} and {secondNumber} is {firstNumber + secondNumber}")
+print(f"Substraction of {firstNumber} and {secondNumber} is {firstNumber-secondNumber}")
+print(f"Multiplication of {firstNumber} and {secondNumber} is {firstNumber*secondNumber}")
+print(f"Division of {firstNumber} and {secondNumber} is {firstNumber/secondNumber}")
+print(f"Remainder of {firstNumber} and {secondNumber} is {firstNumber%secondNumber}")
+
