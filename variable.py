257f18b89987c17c958306ae49c78cd3972a1a3e
Anubhav-Sah <anupsah2005@gmail.comgit>
2025-01-19 12:59:52 +0530

    variable and datatype in python

---
+++ variable.py
@@ -0,0 +1,78 @@
+a=1
+print(a)
+
+print(type (a))
+b="Anubhav"
+
+
+print(b)
+
+print(type (b))
+
+d= None
+
+print(d)
+print (type (d))
+
+char = 'a'
+print(char)
+print (type (char))
+
+# In python all datatype is an object
+
+#*****************Variable*****************
+
+#we can define variable in python just by using name 
+#syntax is " var_name = value "
+#we can assign any data type to variable
+#we can assign any value to variable
+
+#*****************type()*****************
+
+#if we want to check the datatype of the variable then we can use type() function in python
+#type() function returns the data type of the variable
+#we can use print() function to print the value of the variable
+
+
+
+#*****************datatype*****************
+
+#Numeric data : int, float,complex
+#   @ int: 3,-8,0
+#   @ float: 3.14,-8.9,0.0
+#   @ complex: 3+4j,-8-9j,0+0j
+
+#Text data:str
+#   @ str:"Hello wold",'a'
+
+
+#Boolean data: 
+#   @ Boolean data consists of value True and false
+
+
+# Sequence data : list, tuple
+#   @ list: [1,2,3],[1,2,3,4,5]
+#   A list is an ordered collection of data with element seperated by a comma and enclosed within square brackets[].List are mutable and can be modified after creation
+
+list1 = [8,2.3,[4,3],["apple","banana"]]
+print(list1)
+
+print (type (list1))
+
+
+#   @ tuple: (1,2,3),(1,2,3,4,5)
+#   A tuple is an ordered collection of data with element seperated by a comma and enclosed within parentheses.Tuples are immutable and can not be modified after creation
+
+tuple1 = ("Hello",(1,3),"ANUBHAV") 
+print(tuple1)
+print(type(tuple1))
+
+
+
+
+# Mapped data : dict
+#   @ dict: {"name":"anubhav","age":20}
+#   @ A dictionary is an unordered collection of data cointaining a key: value pairs enclosed within curly brackets{}.Dictionaries are mutable and can be modified after creation
+
+
+
